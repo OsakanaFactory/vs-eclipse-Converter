f2e2275609c55e8fb44132cd71ad6d05e8ec94ec
Same <OsakanaFactory@gmail.com>
2023-11-28 12:44:33 +0900

    Add vscode settings for Java project

--- main.py
+++ main.py
@@ -9,23 +9,42 @@ import subprocess
 import json
 
 def check_eclipse_project(project_path):
+    # eclipseで作成されたJavaプロジェクトかどうかチェックする関数
     # .classpathと.projectファイルが存在するかチェック
     if os.path.exists(project_path + "/.classpath") and os.path.exists(project_path + "/.project"):
         # exlipseで作成されたプロジェクトの場合
-        print("\"" + project_path + "\"はEclipseで作成されたプロジェクトです。処理を開始します。")
+        log("\"" + project_path + "\"はEclipseで作成されたプロジェクトです。処理を開始します。")
         return True
     else:
-        print ("\"" + project_path + "\"はEclipseで作成されたプロジェクトではありません。")
+        log ("\"" + project_path + "\"はEclipseで作成されたプロジェクトではありません。")
         return False
         
-    
 
 # コマンドの設定をする関数
 def command():
+    # project_pathをグローバル変数として扱う
+    global project_path
+    # python main.py [project_path]で実行されたときの処理
     project_path = sys.argv[1]
     return project_path
 
+def log(text):
+    # ログに日時を追加して出力する関数
+    date = subprocess.check_output("date", shell=True)
+    print(date.decode("utf-8") + "::" + text)
+
+# vscode用のJavaプロジェクトに必要なファイルを追加する関数
+def add_vscode_file():
+    # project_pathに.vscodeフォルダが存在しない場合のみ作成
+    vscode_folder = project_path + "/.vscode"
+    if not os.path.exists(vscode_folder):
+        os.makedirs(vscode_folder)
+        log(vscode_folder + "に.vscodeフォルダを作成しました。")
+    
 
 check_eclipse_project(command())
 
+
+
+ 
 
